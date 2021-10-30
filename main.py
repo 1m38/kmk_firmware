09f7b7cd20c46528a3571b77e1b4441209105dbf
1m38 <13414029+1m38@users.noreply.github.com>
2021-10-30 17:35:53 +0900

    4keys

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ from kmk.matrix import DiodeOrientation
 
 keyboard = KMKKeyboard()
 
-keyboard.col_pins = (board.D2,)
-keyboard.row_pins = (board.D3,)
+keyboard.col_pins = (board.GP3,board.GP4)
+keyboard.row_pins = (board.GP6,board.GP7)
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
 keyboard.keymap = [
-    [KC.A,]
+    [
+        KC.A, KC.B,
+        KC.C, KC.D
+    ]
 ]
 
 if __name__ == '__main__':
